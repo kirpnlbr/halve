8d80dd858ad9c6d24ec2ad3950fcaf74f1c2a50f
Kir Peñalber <penalberkirstine@gmail.com>
2024-12-29 18:57:06 +0800

    feat: create bill pages

---
+++ app/bills/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function ViewBill() {
+    return (
+        <div></div>
+    )
+}
---
+++ app/bills/new/page.tsx
@@ -0,0 +1,5 @@
+export default function CreateBill() {
+    return (
+        <div>hi</div>
+    )
+}
